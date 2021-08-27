f9c854ab92e4b977fbd6d7489b4f795f1accc95a
kokoa <aikawa_k@mti.co.jp>
2021-08-27 10:52:56 +0900

    :sparkles: 学生用プロジェクトを作成する

---
+++ src/assets/config.js
@@ -0,0 +1,2 @@
+export const baseUrl = "https://2bw6rhfn55.execute-api.ap-northeast-1.amazonaws.com/dev";
+// 自分のエンドポイントに置き換えてください
---
+++ src/views/Home.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="ui main container">
+      <!-- 基本的なコンテンツはここに記載する -->
+      <div class="ui segment">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { baseUrl } from '@/assets/config.js';
+// import axios from "axios";
+// 必要なものはここでインポートする
+// @は/srcと同じ意味です
+// import something from '@/components/something.vue';
+
+export default {
+  name: 'Home',
+  components: {
+   // 読み込んだコンポーネント名をここに記述する
+  },
+  data() {
+    // Vue.jsで使う変数はここに記述する
+    return {
+      post: {
+        text: null,
+        category: null,
+        err: null
+      },
+      search: {
+        username: null,
+        category: null,
+        start: null,
+        end: null,
+        err: null,
+      },
+      query: {
+        start: null,
+        end: null
+      },
+      articles: [],
+      iam: null
+    };
+  },
+  computed: {
+  // 計算した結果を変数として利用したいときはここに記述する
+    /*
+    sortedArticles() {
+    }
+    */
+  },
+  created() {
+    // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
+    this.iam = localStorage.getItem("userId");
+    // apiからarticleを取得する
+    this.getArticles();
+  },
+  methods: {
+    // Vue.jsで使う関数はここで記述する
+    /*
+    getArticles() {
+    },
+    postArticle() {
+    },
+    searchArticle() {
+    },
+    deleteArticle(article) {
+    },
+    fromUnixtoDate(timestamp) {
+    }
+    */
+  }
+}
+</script>
+<style scoped>
+</style>
