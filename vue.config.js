206a08b46e93ed2bb09cfde6aa109c3680072ce6
Ryota Konabe <rt.konabe@gmail.com>
2022-08-17 01:27:10 +0000

    keep working in local.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
+const isOnCloud9 = process.env.C9_PID !== undefined;
+
+const devServerConfig = {
+    disableHostCheck: true
+}
+if (isOnCloud9) {
+    devServerConfig.public = `https://${process.env.C9_PID}.vfs.cloud9.ap-northeast-1.amazonaws.com`;
+}
+
 module.exports = {
     publicPath: "./",
-    "devServer" : {
-        public : `https://${process.env.C9_PID}.vfs.cloud9.ap-northeast-1.amazonaws.com`,
-        disableHostCheck: true
-    }
+    "devServer": devServerConfig
 }
