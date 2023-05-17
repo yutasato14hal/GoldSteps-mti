d85ef3ddfb46c9d9eeff55009829347cb4228fea
kokiai <lplj2120@gmail.com>
2023-05-17 03:37:23 +0000

    add deploy_all command and integrate update to deploy command
    to make it simpler

---
+++ utils.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+name=$(cat yourname.env | sed s/YOURNAME=//)
+
+writename(){
+    if [ "$(ls lambda/ | grep "${name}")" ]; then
+        echo "既にあなたの名前に変わっているため、変更は行いません"
+    else
+        cd lambda
+        for jsf in *.js
+        do
+            mv "${jsf}" "${name}-${jsf}"
+        done
+        echo "ファイル名の変更が完了しました"
+        cd ..
+    fi
+}
+
+lambda_deploy() {
+        
+    # ファイル名が未指定の場合は処理を中断します。
+    if [ $# -lt 1 ]; then
+        echo "デプロイしたいファイル名を指定しましょう Usage: ./utils.sh lambda_deploy <FILENAME>"
+        exit 1
+    fi
+    
+    filename=$1
+    if [ "$(ls lambda/ | grep -x "${filename}")" ]; then
+        echo "関数${filename}の処理を開始します"
+    else
+        # 指定したファイル名が存在しない場合は処理を中断します。
+        echo "${filename}は/lambdaに存在しないようです 存在するファイル名を指定しましょう"
+        exit 1
+    fi
+    
+    cd lambda
+    funame=$(echo $filename | sed s/.js//)
+    aws lambda get-function --function-name $funame > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        # functionが存在している時
+        echo "関数${funame}の内容を更新します"
+        zip -r ${funame}.zip ${filename}
+        aws lambda update-function-code  --function-name ${funame} \
+        --zip-file fileb://${funame}.zip
+        rm ${funame}.zip
+            
+    else
+        # functionが存在していない時
+        echo "関数${funame}のデプロイを開始します"
+        zip -r ${funame}.zip ${filename}
+        aws lambda create-function  --function-name ${funame} \
+        --runtime nodejs16.x \
+        --role arn:aws:iam::347867041416:role/internship_exec_role \
+        --handler ${funame}.handler  --zip-file fileb://${funame}.zip \
+        --region ap-northeast-1
+        rm ${funame}.zip
+    fi
+    cd ..
+    
+}
+
+lambda_deploy_all(){
+
+    cd lambda
+
+    for lambdaf in *.js
+    do
+        funame=$(echo $lambdaf | sed s/.js//)
+        aws lambda get-function --function-name $funame > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            # functionが存在している時
+            echo "関数${funame}の内容を更新します"
+            zip -r ${funame}.zip ${lambdaf}
+            aws lambda update-function-code  --function-name ${funame} \
+            --zip-file fileb://${funame}.zip
+            rm ${funame}.zip
+            
+        else
+            # functionが存在していない時
+            echo "関数${funame}のデプロイを開始します"
+            zip -r ${funame}.zip ${lambdaf}
+            aws lambda create-function  --function-name ${funame} \
+            --runtime nodejs16.x \
+            --role arn:aws:iam::347867041416:role/internship_exec_role \
+            --handler ${funame}.handler  --zip-file fileb://${funame}.zip \
+            --region ap-northeast-1
+            rm ${funame}.zip
+        fi
+    done
+    cd ..
+}
+
+s3_deploy(){
+    aws s3 mb s3://${name}-internship
+}
+
+s3_update(){
+    npm run build
+    aws s3 cp ./dist s3://${name}-internship --recursive --acl public-read
+}
+
+deletename(){
+    cd lambda
+    for noname in *.js
+    do
+        blankp=$(echo ${noname} | sed s/${name}-//)
+        mv "${noname}" "${blankp}"
+    done
+    echo "名前を削除しました"
+    cd ..
+}
+
+case $1 in
+    "") writename;;
+    lambda_deploy_all) lambda_deploy_all;;
+    lambda_deploy) lambda_deploy $2;;
+    s3_deploy) s3_deploy;;
+    s3_update) s3_update;;
+    delete_name) deletename;;
+esac
