e13d13e8aef9f84f4cb717d480fb77de63ddd5a7
Karen Kijima <k.renren0119@gmail.com>
2023-08-21 03:26:55 +0000

    2日目までの状態

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <Menu />
     <router-view/>
   </div>
 </template>
+
+<script>
+import { RouterView } from 'vue-router';
+import Menu from '@/components/Menu.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // 読み込んだコンポーネント名をここに記述する
+    Menu
+  },
+}
+</script>
