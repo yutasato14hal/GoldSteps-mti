e46b27f860f6fe2e842433fc23f9bdf682790c8f
Tsumugi Isogami <isomugi2659@icloud.com>
2023-08-23 09:43:32 +0000

    can put 0 to the exp column

--- lambda/user-create-internship.js
+++ lambda/user-create-internship.js
@@ -1,7 +1,7 @@
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 const { marshall } = require("@aws-sdk/util-dynamodb");
 const client = new DynamoDBClient({ region: "ap-northeast-1" });
-const TableName = "User";
+const TableName = "team2_user";
 
 /*** 通常版の解答例(発展課題を含む最終版は下にあります。) ***/
 exports.handler = async (event, context) => {
@@ -15,15 +15,16 @@ exports.handler = async (event, context) => {
 
   // { varName }のような形式を分割代入と呼び、右側のオブジェクトの中からvarNameプロパティを変数varNameとして切り出すことができる
   // (https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
-  const { userId, age, nickname, password } = JSON.parse(event.body);
+  const { userId, password ,age } = JSON.parse(event.body);
+  console.log(event.body);
   const param = {
     // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。
     TableName, // TableName: TableNameと同じ意味
     Item: marshall({
       userId, // userId: userIdと同じ意味
       age, // age: ageと同じ意味
-      nickname, // nickname: nicknameと同じ意味
       password, // password: passwordと同じ意味
+      "exp":0
     }),
   };
 
@@ -32,7 +33,7 @@ exports.handler = async (event, context) => {
   try {
     await client.send(command);
     response.statusCode = 201;
-    response.body = JSON.stringify({ userId, age, nickname });
+    response.body = JSON.stringify({ userId, age });
   } catch (e) {
     response.statusCode = 500;
     response.body = JSON.stringify({
@@ -45,58 +46,58 @@ exports.handler = async (event, context) => {
 };
 
 /*** 発展課題も含む最終版 ***/
-exports.handler = async (event, context) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({ message: "" }),
-  };
+// exports.handler = async (event, context) => {
+//   const response = {
+//     statusCode: 200,
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//     },
+//     body: JSON.stringify({ message: "" }),
+//   };
 
-  const body = event.body ? JSON.parse(event.body) : null;
-  if (!body || !body.userId || !body.age || !body.nickname || !body.password) {
-    response.statusCode = 400;
-    response.body = JSON.stringify({
-      message:
-        "無効なリクエストです。request bodyに必須パラメータがセットされていません。",
-    });
+//   const body = event.body ? JSON.parse(event.body) : null;
+//   if (!body || !body.userId || !body.age || !body.password) {
+//     response.statusCode = 400;
+//     response.body = JSON.stringify({
+//       message:
+//         "無効なリクエストです。request bodyに必須パラメータがセットされていません。",
+//     });
 
-    return response;
-  }
+//     return response;
+//   }
 
-  // { varName }のような形式を分割代入と呼び、右側のオブジェクトの中からvarNameプロパティを変数varNameとして切り出すことができる
-  // (https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
-  const { userId, age, nickname, password } = body;
-  const param = {
-    // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。
-    TableName, // TableName: TableNameと同じ意味
-    Item: marshall({
-      userId, // userId: userIdと同じ意味
-      age, // age: ageと同じ意味
-      nickname, // nickname: nicknameと同じ意味
-      password, // password: passwordと同じ意味
-    }),
-  };
+//   // { varName }のような形式を分割代入と呼び、右側のオブジェクトの中からvarNameプロパティを変数varNameとして切り出すことができる
+//   // (https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
+//   const { userId, age, password } = body;
+//   const param = {
+//     // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。
+//     TableName, // TableName: TableNameと同じ意味
+//     Item: marshall({
+//       userId, // userId: userIdと同じ意味
+//       age, // age: ageと同じ意味
+//       exp:0, // nickname: nicknameと同じ意味
+//       password, // password: passwordと同じ意味
+//     }),
+//   };
 
-  const command = new PutItemCommand(param);
+//   const command = new PutItemCommand(param);
 
-  try {
-    await client.send(command);
-    response.statusCode = 201;
-    response.body = JSON.stringify({
-      userId,
-      age,
-      nickname,
-      token: "mtiToken",
-    });
-  } catch (e) {
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: "予期せぬエラーが発生しました。",
-      errorDetail: e.toString(),
-    });
-  }
+//   try {
+//     await client.send(command);
+//     response.statusCode = 201;
+//     response.body = JSON.stringify({
+//       userId,
+//       age,
+//       nickname,
+//       token: "mtiToken",
+//     });
+//   } catch (e) {
+//     response.statusCode = 500;
+//     response.body = JSON.stringify({
+//       message: "予期せぬエラーが発生しました。",
+//       errorDetail: e.toString(),
+//     });
+//   }
 
-  return response;
-};
+//   return response;
+// };
