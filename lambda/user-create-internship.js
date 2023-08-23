3f0bfebad131902c110a8de22bc0779b906473d3
Tsumugi Isogami <isomugi2659@icloud.com>
2023-08-23 10:10:33 +0000

    change to later one

--- lambda/user-create-internship.js
+++ lambda/user-create-internship.js
@@ -4,48 +4,6 @@ const client = new DynamoDBClient({ region: "ap-northeast-1" });
 const TableName = "team2_user";
 
 /*** 通常版の解答例(発展課題を含む最終版は下にあります。) ***/
-exports.handler = async (event, context) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({ message: "" }),
-  };
-
-  // { varName }のような形式を分割代入と呼び、右側のオブジェクトの中からvarNameプロパティを変数varNameとして切り出すことができる
-  // (https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
-  const { userId, password ,age } = JSON.parse(event.body);
-  console.log(event.body);
-  const param = {
-    // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。
-    TableName, // TableName: TableNameと同じ意味
-    Item: marshall({
-      userId, // userId: userIdと同じ意味
-      age, // age: ageと同じ意味
-      password, // password: passwordと同じ意味
-      "exp":0
-    }),
-  };
-
-  const command = new PutItemCommand(param);
-
-  try {
-    await client.send(command);
-    response.statusCode = 201;
-    response.body = JSON.stringify({ userId, age });
-  } catch (e) {
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: "予期せぬエラーが発生しました。",
-      errorDetail: e.toString(),
-    });
-  }
-
-  return response;
-};
-
-/*** 発展課題も含む最終版 ***/
 // exports.handler = async (event, context) => {
 //   const response = {
 //     statusCode: 200,
@@ -55,28 +13,18 @@ exports.handler = async (event, context) => {
 //     body: JSON.stringify({ message: "" }),
 //   };
 
-//   const body = event.body ? JSON.parse(event.body) : null;
-//   if (!body || !body.userId || !body.age || !body.password) {
-//     response.statusCode = 400;
-//     response.body = JSON.stringify({
-//       message:
-//         "無効なリクエストです。request bodyに必須パラメータがセットされていません。",
-//     });
-
-//     return response;
-//   }
-
 //   // { varName }のような形式を分割代入と呼び、右側のオブジェクトの中からvarNameプロパティを変数varNameとして切り出すことができる
 //   // (https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
-//   const { userId, age, password } = body;
+//   const { userId, password ,age } = JSON.parse(event.body);
+//   console.log(event.body);
 //   const param = {
 //     // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。
 //     TableName, // TableName: TableNameと同じ意味
 //     Item: marshall({
 //       userId, // userId: userIdと同じ意味
 //       age, // age: ageと同じ意味
-//       exp:0, // nickname: nicknameと同じ意味
 //       password, // password: passwordと同じ意味
+//       "exp":0
 //     }),
 //   };
 
@@ -85,12 +33,7 @@ exports.handler = async (event, context) => {
 //   try {
 //     await client.send(command);
 //     response.statusCode = 201;
-//     response.body = JSON.stringify({
-//       userId,
-//       age,
-//       nickname,
-//       token: "mtiToken",
-//     });
+//     response.body = JSON.stringify({ userId, age });
 //   } catch (e) {
 //     response.statusCode = 500;
 //     response.body = JSON.stringify({
@@ -101,3 +44,59 @@ exports.handler = async (event, context) => {
 
 //   return response;
 // };
+
+/*** 発展課題も含む最終版 ***/
+exports.handler = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ message: "" }),
+  };
+
+  const body = event.body ? JSON.parse(event.body) : null;
+  if (!body || !body.userId || !body.age || !body.password) {
+    response.statusCode = 400;
+    response.body = JSON.stringify({
+      message:
+        "無効なリクエストです。request bodyに必須パラメータがセットされていません。",
+    });
+
+    return response;
+  }
+
+  // { varName }のような形式を分割代入と呼び、右側のオブジェクトの中からvarNameプロパティを変数varNameとして切り出すことができる
+  // (https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
+  const { userId, age, password } = body;
+  const param = {
+    // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。
+    TableName, // TableName: TableNameと同じ意味
+    Item: marshall({
+      userId, // userId: userIdと同じ意味
+      age, // age: ageと同じ意味
+      "exp":0, // nickname: nicknameと同じ意味
+      password, // password: passwordと同じ意味
+    }),
+  };
+
+  const command = new PutItemCommand(param);
+
+  try {
+    await client.send(command);
+    response.statusCode = 201;
+    response.body = JSON.stringify({
+      userId,
+      age,
+      token: "",
+    });
+  } catch (e) {
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "予期せぬエラーが発生しました。",
+      errorDetail: e.toString(),
+    });
+  }
+
+  return response;
+};
