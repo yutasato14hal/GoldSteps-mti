d35003297b9e5e18d0ac5fe202abb648a81cbf0f
Karen Kijima <k.renren0119@gmail.com>
2023-08-25 03:16:20 +0000

    fix user_update

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,52 +5,60 @@
       <div class="ui text loader">Loading</div>
     </div>
 
+ 
+
     <!-- Error Message -->
     <div class="ui red message" v-if="errorMsg">
       <p>{{ errorMsg }}</p>
     </div>
 
+ 
+
     <!-- User Status -->
     <div class="ui segment">
-      <h3>User Status</h3>
-      <p><strong>UserId: </strong>{{ user.userId }}</p>
-      <p><strong>Volume: </strong>{{ user.volume }}</p>
-      <p><strong>Experience: </strong>{{ user.exp }}</p>
+      <h3>ユーザー情報</h3>
+      <p><strong>ユーザー名： </strong>{{ user.userId }}</p>
+      <p><strong>運動強度（松竹梅）: </strong>{{ user.volume }}</p>
+      <p><strong>経験値：</strong>{{ user.exp }}</p>
     </div>
+
+ 
 
     <!-- Upgrade to Paid Plan -->
     <div class="ui segment">
-      <h3>Upgrade to Paid Plan</h3>
+      <h3>有料プランへのアップデート</h3>
       <form class="ui form" @submit.prevent="upgradePlan">
         <div class="field">
-          <label>Plan</label>
+          <label>プラン</label>
           <select v-model="user.plan">
-            <option value="free">Free</option>
-            <option value="premium">Premium</option>
+            <option value="free">無料</option>
+            <option value="premium">有料</option>
           </select>
         </div>
-        <button class="ui button green" type="submit">Upgrade</button>
+        <button class="ui button green" type="submit">アップデート</button>
       </form>
     </div>
 
+ 
+
     <!-- Edit Settings -->
     <div class="ui segment">
-      <h3>Edit Settings</h3>
+      <h3>ユーザー情報の編集</h3>
       <form class="ui form" @submit.prevent="updateSettings">
         <div class="field">
-          <label>Password</label>
+          <label>パスワード</label>
           <input type="password" v-model="user.password" />
         </div>
         <div class="field">
-          <label>Strength</label>
-          <select v-model="user.strength">
+          <label>運動強度（松竹梅）</label>
+          <select v-model="user.volume">
             <option disabled value="null">選択してください</option> <!-- 追加 -->
-            <option value="松">松</option>
-            <option value="竹">竹</option>
-            <option value="梅">梅</option>
+            <option value="matu">松</option>
+            <option value="take">竹</option>
+            <option value="ume">梅</option>
           </select>
         </div>
-        <button class="ui button blue" type="submit">Update</button>
+        <button class="ui button blue" type="submit">更新</button>
       </form>
     </div>
   </div>
@@ -84,39 +92,7 @@
       },
       // Other methods (Upgrade, Settings Update)
       // (snip)
-    },
-    async created() {
-      this.isCallingApi = true;
-
-      try {
-        const userId = window.localStorage.userId; // userIdを設定。
-        const res = await fetch(`${baseUrl}/user?userId=${userId}`, {
-          method: 'GET',
-          headers
-        });
-
-        const text = await res.text();
-        const jsonData = text ? JSON.parse(text) : {};
-
-        if (!res.ok) {
-          const errorMessage = jsonData.message ?? 'エラーメッセージがありません';
-          throw new Error(errorMessage);
-        }
-
-        this.user = {
-          userId: jsonData.userId ?? '',
-          volume: jsonData.volume ?? '',
-          exp: jsonData.exp ?? '',
-          plan: jsonData.plan ?? 'free'
-        };
-      }
-      catch (e) {
-        this.errorMsg = `ユーザーステータス取得時にエラーが発生しました: ${e}`;
-      }
-      finally {
-        this.isCallingApi = false;
-      }
-    },
+    
      clearError() {
     this.errorMsg = '';
   },
@@ -152,13 +128,21 @@
     console.log("update")
     this.isCallingApi = true;
     try {
+      const userId = window.localStorage.userId;
+      const { password, volume } = this.user;
+      const reqBody = {
+        userId,
+        password,
+        volume,
+      };
+      
       const res = await fetch(baseUrl + '/user', {
         method: 'PUT',
         headers: {
           ...headers,
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(this.user)
+        body: JSON.stringify(reqBody)
       });
 
       const text = await res.text();
@@ -180,6 +164,39 @@
     }
   },
   // ...その他のメソッド
+  },
+  created: async function() {
+      this.isCallingApi = true;
+
+      try {
+        const userId = window.localStorage.userId; // userIdを設定。
+        const res = await fetch(`${baseUrl}/user?userId=${userId}`, {
+          method: 'GET',
+          headers
+        });
+
+        const text = await res.text();
+        const jsonData = text ? JSON.parse(text) : {};
+
+        if (!res.ok) {
+          const errorMessage = jsonData.message ?? 'エラーメッセージがありません';
+          throw new Error(errorMessage);
+        }
+
+        this.user = {
+          userId: jsonData.userId ?? '',
+          volume: jsonData.volume ?? '',
+          exp: jsonData.exp ?? '',
+          plan: jsonData.plan ?? 'free'
+        };
+      }
+      catch (e) {
+        this.errorMsg = `ユーザーステータス取得時にエラーが発生しました: ${e}`;
+      }
+      finally {
+        this.isCallingApi = false;
+      }
+    },
 }
 
 
