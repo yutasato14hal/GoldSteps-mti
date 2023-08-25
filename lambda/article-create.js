202e8158030ff9ed20839edbda450ae4a00e8c18
Karen Kijima <k.renren0119@gmail.com>
2023-08-25 00:56:08 +0000

    fic article-create

--- lambda/article-create.js
+++ lambda/article-create.js
@@ -11,12 +11,22 @@ exports.handler = async (event, context) => {
     },
     body: JSON.stringify({ message: "" }),
   };
+  
+  if (event.headers.authorization !== "mtiToken") {
+    response.statusCode = 401;
+    response.body = JSON.stringify({
+      message: "認証されていません。HeaderにTokenを指定してください",
+    });
+
+    return response;
+  }
+
  
   // TODO: リクエストボディの中身をJavaScriptオブジェクトに変換し、1つ、あるいは複数の変数に代入する
-  const {text, timestamp} = event.body ? JSON.parse(event.body) : null;
+  const body = event.body ? JSON.parse(event.body) : null;
   
   
-  if (!timestamp || !text){
+  if (!body || !body.userId || !body.text){
     response.statusCode = 400;
     response.body = JSON.stringify({
       message : "無効なリクエストです。request bodyに必須パラメータがセットされていません。"
@@ -24,15 +34,16 @@ exports.handler = async (event, context) => {
     return response;
   }
  
-  // TODO: DBに登録するための情報をparamオブジェクトとして宣言する（中身を記述）
-  const userId = event.queryStringParameter.userId;
+  
+  const { userId, text } = body;
+  const timestamp = Date.now();
   const param = {
-    TableName,
-    "Item" : marshall({
-      userId,
-      text,
-      timestamp
-    })
+    TableName, 
+    Item: marshall({
+      userId, 
+      text, 
+      timestamp,
+    }),
   };
  
   // DBにデータを登録するコマンドを用意
@@ -43,7 +54,7 @@ exports.handler = async (event, context) => {
     await client.send(command);
     // TODO: 登録に成功した場合の処理を記載する。(status codeの設定と、response bodyの設定)
     response.statusCode = 200;
-    response.body = JSON.stringify({userId,timestamp,text});
+    response.body = JSON.stringify({userId,text,timestamp});
   } catch (e) {
     response.statusCode = 500;
     response.body = JSON.stringify({
